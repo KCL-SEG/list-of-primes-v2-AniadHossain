8a65ef3a4e2ca24534a7d5165ba8eb40bf200e5b
Aniad <aniad.hossain@kcl.ac.uk>
2023-09-27 14:53:47 +0100

    Complete task

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if(number_of_primes < 1):
+        raise ValueError('ENTER AN INTEGER GREATER THAN 0')
     list = []
+    count = 0
+    num = 2
+
+    while count < number_of_primes:
+        if(isPrime(num)):
+            list.append(num)
+            count += 1
+        num += 1
+
+
     return list
+
+def isPrime(num):
+    for i in range(2,num,1):
+        if num % i == 0:
+            return False
+    return True
